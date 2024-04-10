c637b170e5c2bf1143cd993bd1e7ad33e12660a6
Xingyuan Zhang <wizardicarus@gmail.com>
2024-04-10 14:55:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='distracting_control',
     py_modules=['distracting_control'],
+    packages=find_packages(),
     version='1.0.0',
     install_requires=[
         'dm-control',
